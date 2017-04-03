e24f8d9d33a6bd746e80901b8834d8ca1872d0ff
Ian Hogan <hoganmathematics@gmail.com>
2017-04-03 13:00:01 -0400

    Debugging to runable state.

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,32 +1,40 @@
 //A linked list implementation in C++
 #include<iostream>
+//#include<string>
 using std::cout;
 using std::cin;
 using std::string;
 
-template <class T>
-class Node{
-private:
-  T data;
-  Node *next;
-public:
-  //some public things, perhaps an accessor
-}
+//template <class T>
+struct node{
+  //T data;
+  string data;
+  node *next;
+
+  node(string initdata){
+    data=initdata;
+  }//regular constructor; or just set from outside?
+  node(){
+    data="";
+    
+  }
+};
 
 class LinkedList{
 private:
-  Node head;
+  node head;
+  node tail;//one way of speeding up push
 public:
-  LinkedList(T initdata){
-    Node<T> head=Node(initdata)
+  LinkedList(string initdata){
+    head.data=initdata;
 
   }//regular constructor
   ~LinkedList(){
-    //will need to walk the list and destruct all the nodes
+    //will need to walk the list and destruct all the nodes.
     //It would be clever to have a node terminator send the
     //terminator message to the next node
   }//destructor
-}//class LinkedList
+}; //class LinkedList
 
 
 
